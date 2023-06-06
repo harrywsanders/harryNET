54d5811e8418a49307b40c6e552b628e2314f7d0
harry <hwsand@umich.edu>
2023-06-05 22:34:36 -0400

    improved arguments with a struct

---
+++ CommandLine.h
@@ -0,0 +1,26 @@
+struct Options {
+    std::string trainingDataPath;
+    std::string testDataPath;
+    int numEpochs;
+    double learningRate;
+    int patience;
+};
+
+Options parseCommandLineArgs(int argc, char* argv[]) {
+    // Set default values
+    Options options;
+    options.trainingDataPath = "train.csv";
+    options.testDataPath = "test.csv";
+    options.numEpochs = 100;
+    options.learningRate = 0.01;
+    options.patience = 10;
+
+    // Override with command line arguments
+    if (argc > 1) options.trainingDataPath = argv[1];
+    if (argc > 2) options.testDataPath = argv[2];
+    if (argc > 3) options.numEpochs = std::stoi(argv[3]);
+    if (argc > 4) options.learningRate = std::stod(argv[4]);
+    if (argc > 5) options.patience = std::stoi(argv[5]);
+
+    return options;
+}
--- main.cpp
+++ main.cpp
@@ -10,6 +10,7 @@
 #include 'neuron.h'
 #include 'layers.h'
 #include 'NeuralNet.h'
+#include 'CommandLine.h'
 
 
 void loadDataset(const std::string& filename, std::vector<std::vector<double>>& inputs, std::vector<std::vector<double>>& outputs) {
@@ -38,41 +39,19 @@ void loadDataset(const std::string& filename, std::vector<std::vector<double>>& 
 }
 
 int main(int argc, char* argv[]) {
-    // Set default values
-    std::string trainingDataPath = "train.csv";
-    std::string testDataPath = "test.csv";
-    int numEpochs = 100;
-    double learningRate = 0.01;
-    int patience = 10;
-
-    // Override with command line arguments
-    if (argc > 1) {
-        trainingDataPath = argv[1];
-    }
-    if (argc > 2) {
-        testDataPath = argv[2];
-    }
-    if (argc > 3) {
-        numEpochs = std::stoi(argv[3]);
-    }
-    if (argc > 4) {
-        learningRate = std::stod(argv[4]);
-    }
-    if (argc > 5) {
-        patience = std::stoi(argv[5]);
-    }
-
     if (argc > 6) {
         std::cout << "Usage: " << argv[0] << " [<training_data>] [<test_data>] [<num_epochs>] [<learning_rate>] [<patience>]" << std::endl;
         return 1;
     }
 
+    Options options = parseCommandLineArgs(argc, argv);
+
     std::vector<std::vector<double>> trainingInputs, trainingOutputs;
     std::vector<std::vector<double>> testInputs, testOutputs;
 
     // Load datasets
-    loadDataset(trainingDataPath, trainingInputs, trainingOutputs);
-    loadDataset(testDataPath, testInputs, testOutputs);
+    loadDataset(options.trainingDataPath, trainingInputs, trainingOutputs);
+    loadDataset(options.testDataPath, testInputs, testOutputs);
 
     // Initialize and train the network
     NeuralNetwork network;
@@ -80,7 +59,7 @@ int main(int argc, char* argv[]) {
     network.layers.push_back(Layer(50));
     network.layers.push_back(Layer(10));
 
-    network.train(trainingInputs, trainingOutputs, testInputs, testOutputs, learningRate, numEpochs, patience);
+    network.train(trainingInputs, trainingOutputs, testInputs, testOutputs, options.learningRate, options.numEpochs, options.patience);
 
     // Measure accuracy on the test set
     double testAccuracy = network.accuracy(testInputs, testOutputs);
@@ -88,5 +67,3 @@ int main(int argc, char* argv[]) {
 
     return 0;
 }
-
-
