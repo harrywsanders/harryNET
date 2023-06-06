e0cc221566bfa2cec4053cee52df605c6c3fc406
harry <hwsand@umich.edu>
2023-06-05 21:43:15 -0400

    formatting

--- main.cpp
+++ main.cpp
@@ -1,49 +1,58 @@
 #include <vector>
 #include <cmath>
 
-class Neuron {
+class Neuron
+{
 public:
     std::vector<double> weights; // weights for each input
-    double bias; // bias value
-    double output; // output value
-    double delta; // delta value for backpropagation
+    double bias;                 // bias value
+    double output;               // output value
+    double delta;                // delta value for backpropagation
 };
 
-class Layer {
+class Layer
+{
 public:
     std::vector<Neuron> neurons;
 };
 
-class NeuralNetwork {
+class NeuralNetwork
+{
 public:
     std::vector<Layer> layers;
 
-        void forwardPropagate(std::vector<double>& inputs) {}
+    void forwardPropagate(std::vector<double> &inputs) {}
 
-    void backPropagate(std::vector<double>& targetOutputs) {}
+    void backPropagate(std::vector<double> &targetOutputs) {}
 
     void updateWeightsAndBiases(double learningRate) {}
 };
 
-void NeuralNetwork::forwardPropagate(std::vector<double>& inputs){
-    for (auto& layer : layers) {
-            std::vector<double> outputs;
-            for (auto& neuron : layer.neurons) {
-                double activation = neuron.bias;
-                for (int i = 0; i < neuron.weights.size(); i++) {
-                    activation += neuron.weights[i] * inputs[i];
-                }
-                neuron.output = 1 / (1 + std::exp(-activation)); // sigmoid activation function
-                outputs.push_back(neuron.output);
+void NeuralNetwork::forwardPropagate(std::vector<double> &inputs)
+{
+    for (auto &layer : layers)
+    {
+        std::vector<double> outputs;
+        for (auto &neuron : layer.neurons)
+        {
+            double activation = neuron.bias;
+            for (int i = 0; i < neuron.weights.size(); i++)
+            {
+                activation += neuron.weights[i] * inputs[i];
             }
-            inputs = outputs; // outputs of this layer are inputs to the next layer
+            neuron.output = 1 / (1 + std::exp(-activation)); // sigmoid activation function
+            outputs.push_back(neuron.output);
         }
+        inputs = outputs; // outputs of this layer are inputs to the next layer
+    }
 }
 
-void NeuralNetwork::backPropagate(std::vector<double>& targetOutputs) {
+void NeuralNetwork::backPropagate(std::vector<double> &targetOutputs)
+{
     // Calculate output layer deltas
-    Layer& outputLayer = layers.back();
-    for (int i = 0; i < outputLayer.neurons.size(); i++) {
+    Layer &outputLayer = layers.back();
+    for (int i = 0; i < outputLayer.neurons.size(); i++)
+    {
         double output = outputLayer.neurons[i].output;
         double target = targetOutputs[i];
         double error = target - output;
@@ -51,32 +60,40 @@ void NeuralNetwork::backPropagate(std::vector<double>& targetOutputs) {
     }
 
     // Calculate hidden layer deltas
-    Layer& hiddenLayer = layers[0]; // assuming single hidden layer
-    Layer& nextLayer = layers[1];
-    for (int i = 0; i < hiddenLayer.neurons.size(); i++) {
+    Layer &hiddenLayer = layers[0]; // assuming single hidden layer
+    Layer &nextLayer = layers[1];
+    for (int i = 0; i < hiddenLayer.neurons.size(); i++)
+    {
         double output = hiddenLayer.neurons[i].output;
         double error = 0.0;
-        for (int j = 0; j < nextLayer.neurons.size(); j++) {
+        for (int j = 0; j < nextLayer.neurons.size(); j++)
+        {
             error += nextLayer.neurons[j].delta * nextLayer.neurons[j].weights[i]; // weights from hidden layer to output layer
         }
         hiddenLayer.neurons[i].delta = error * output * (1 - output); // derivative of sigmoid
     }
 }
 
-void NeuralNetwork::updateWeightsAndBiases(double learningRate) {
-     std::vector<double> inputs;
-        if (layers.size() > 1) {
-            for (auto& neuron : layers[layers.size() - 2].neurons) {
-                inputs.push_back(neuron.output);
-            }
-        }
-
-        for (auto& layer : layers) {
-            for (auto& neuron : layer.neurons) {
-                for (int i = 0; i < neuron.weights.size(); i++) {
-                    neuron.weights[i] += learningRate * neuron.delta * inputs[i]; // update weight
-                }
-                neuron.bias += learningRate * neuron.delta; // update bias
-            }
+void NeuralNetwork::updateWeightsAndBiases(double learningRate)
+{
+    std::vector<double> inputs;
+    if (layers.size() > 1)
+    {
+        for (auto &neuron : layers[layers.size() - 2].neurons)
+        {
+            inputs.push_back(neuron.output);
         }
     }
+
+    for (auto &layer : layers)
+    {
+        for (auto &neuron : layer.neurons)
+        {
+            for (int i = 0; i < neuron.weights.size(); i++)
+            {
+                neuron.weights[i] += learningRate * neuron.delta * inputs[i]; // update weight
+            }
+            neuron.bias += learningRate * neuron.delta; // update bias
+        }
+    }
+}
